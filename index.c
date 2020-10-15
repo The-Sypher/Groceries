9a3f6277781166d23e348cc337a1423f4decdf1e
rishit-desai <rishit-desai@users.noreply.github.com>
2020-10-15 18:30:13 +0530

    Bohot kuch kiya hai dekh lo

--- index.c
+++ index.c
@@ -2,7 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-int indx;
+int indx = -1;
+int mem_index;
 struct INVENTORY
 {
   char item_name[50];
@@ -28,7 +29,7 @@ struct INVOICE
   float discount;
   float total;
   int item_number[53];
-  int item_quantity[53];
+  int item_q[53];
   char cust_name[50];
 }bill;
 
@@ -124,6 +125,7 @@ void goodjob(int id)
     }
   }*/
 }
+
 void showsales(int id)
 {
   FILE *emp;
@@ -141,16 +143,15 @@ void showsales(int id)
   }
 }
 
-
-
 void update_inventory()
 {
   int i;
   for(i=0;i<54;i++)
   {
-    item[i].item_quantity = item[i].item_quantity - bill.item_quantity[i];
+    item[i].item_quantity = item[i].item_quantity - bill.item_q[i];
   }
 }
+
 bool check_if_possible(int item_number, int quantity)
 {
   if(item[item_number-101].item_quantity >=  quantity)
@@ -162,6 +163,7 @@ bool check_if_possible(int item_number, int quantity)
     return false;
   }
 }
+
 void member_reading()
 {
   FILE *member;
@@ -263,53 +265,51 @@ void update_item_quantity_after_sale()
   int i;
   for(i=0;i<54;i++)
   {
-    item[bill.item_number[i] - 101].item_quantity = item[bill.item_number[i] - 101].item_quantity - bill.item_quantity[i];
+    item[bill.item_number[i] - 101].item_quantity = item[bill.item_number[i] - 101].item_quantity - bill.item_q[i];
   }
   inventory_updation();
 }
+
 void add_item(int id, int quantity)
 {
   if(check_if_possible(id,quantity))
   {
-    printf("ITEM ADDED!!\n");
-    bill.item_quantity[indx]=quantity;
-    bill.item_number[indx]=id;
     indx++;
+    printf("ITEM ADDED!!\n");
+    bill.item_q[indx]=quantity;
+    bill.item_number[indx]=id;
   }
   else
   {
-    printf("NOT ENOUGH ITEMS AVAILABLE!!\n");
+    printf("Not enough items available!!\n");
   }
 }
 
-
 void print_bill()
 {
+    int i;
     for(i=0;i<indx;i++)
     {
-        bill.subtotal=item[bill.item_number[i]-101].item_MRP*bill.item_q[i];
+        bill.subtotal += item[bill.item_number[i]-101].item_MRP*bill.item_q[i];
     }
     bill.gst=bill.subtotal*0.18;
     bill.total=bill.subtotal+bill.gst;
 
     char a[]="Item Name";
     char b[]="Item Number";
-    char c[]="Units";
-    char d[]="MRP";
-    char e[]="Our Price";
+    char c[]="Quantity";
+    char d[]="Rate";
+    char e[]="Amount";
     char f[]="Total";
     char g[]="Subtotal";
     char h[]="GST";
-    int i,num=5;
     printf("|________________________________________________________________________________________________________|\n");
-    printf("|**************************************************INVOICE***********************************************|\n\n");
-
-
-
-    printf("|%20s|%20s|%20s|%20s|%20s|\n",a,b,c,d,e);
-    for(i=0;i<num;i++){
-    printf("|%20s|%20d|%20d|%20.2f|%20.2f|\n",item[0].item_name,item[bill.item_number[0]-101].item_no,bill.item_q[0],item[bill.item_number[0]-101].item_MRP,item[bill.item_number[0]-101].item_price);
-}
+    printf("|**************************************************Invoice***********************************************|\n\n");
+    printf("|%20s|%20s|%20s|%20s|%20s|\n",b,a,c,d,e);
+    for(i=0;i<indx;i++)
+    {
+      printf("|%20d|%20s|%20d|%20.2f|%20.2f|\n", item[bill.item_number[i] - 101].item_no, item[bill.item_number[i] - 101].item_name, bill.item_q[i], item[bill.item_number[i] - 101].item_MRP, item[bill.item_number[i] - 101].item_MRP * bill.item_q[i]);
+    }
     printf("|---------------------------------------------------------------------------------------------------------\n");
     printf("                                                                                                __________\n");
     printf("%86s%20.2f\n",g,bill.subtotal);
@@ -318,7 +318,10 @@ void print_bill()
     printf("                                                                                                __________\n");
 }
 
+void print_inventory()
+{
 
+}
 
 /*Samarth when building the main after calling the printing of bill call goodjob(id) make an id variable in main and input it inthe start of the program*/
 int main()
@@ -330,81 +333,87 @@ int main()
   //              create bill(discount , gst)
   //check sales
   //2nd screen
-  struct MEMBER a;
   int s, num, k, m, q, t;
-  printf("\t\t\t\t#########  WELCOME HUMAN  ##########\n");
+  char x;
+  printf("\t\t\t\t#########  Welcome Human  ##########\n");
 
-  printf("\n\nENTER EMPLOYEE LOGIN ID: ");
+  printf("\n\nEnter Employee Id:");
   scanf("%d", &num);
   if (num == 007)
   {
     system("cls");
     printf("\n___________________________________________");
-    printf("\n******************MENU*********************");
+    printf("\n******************Menu*********************");
     printf("\n___________________________________________\n ");
     do
     {
 
-      printf("\nMENU:\n1.NEW CUSTOMER \n2.CHECK INVERNTORY \n3.UPDATE INVENTORY \n4.EXIT: ");
+      printf("\nMenu:\n1.New Customer \n2.Check Inventory \n3.Go Back\n4.Exit:\n");
       scanf("%d", &s);
       switch (s)
       {
         case 1:
-        inventory_reading();
-        {
+          inventory_reading();
           system("cls");
           printf("\n__________________________________________");
-          printf("\n****************MENU--NEW CUSTOMER**********");
+          printf("\n****************Menu***************");
           printf("\n____________________________________________\n ");
-          printf("\nMENU/NEW CUSTOMER:\n1.EXISTING MEMBER \n2.ADD MEMBER \n3.GO BACK: ");
+          printf("\n1.Existing Member \n2.Add Member \n3.Go Back: ");
           scanf("%d", &k);
           if (k == 1)
           {
-            printf(" \nEXIST MEMBER\n");
-            printf("ID OF MEMBER##### ");
-            printf(" ADD ITEMSS:\n");
+            printf(" \nExisting Member\n");
+            printf("ID OF MEMBER: ");
+            scanf("%d",&mem_index);
             do
             {
-              printf("ENTER 0 TO PRINT BILL:");
-              printf("ENTER ITEM NUMBER:");
+              printf("Enter 0 To Print Bill\n");
+              printf("Add Items\n");
+              printf("Item number:");
               scanf("%d", &m);
-              printf("ENTER QUANTITY:");
-              scanf("%d", &q);
+              if(m != 0)
+              {
+                printf("Quantity:");
+                scanf("%d", &q);
+              }
               add_item(m, q);
+              system("cls");
             } while (m != 0);
+            print_bill();
+            printf("Press any key to continue.....\n");
+            scanf("%s",&x);
           }
           if (k == 2)
           {
-            printf("\nADD MEMBER\n");
+            printf("\n***Add Member***\n");
             printf("ENTER THE NAME OF MEMBER: ");
-            scanf("%d", &a.name);
             fflush(stdin);
+            scanf("%s", &mem[mem_index].name);
             printf("ENTER THE id OF MEMBER: ");
-            scanf("%d", &a.id);
             fflush(stdin);
-            a.number_of_donation = 0;
+            scanf("%s", &mem[mem_index].id);
+            mem[mem_index].number_of_donation = 0;
           }
-          break;
-        };
+          system("cls");
+        break;
+        
         case 2:
-        {
-          printf("\nCHECK INVENTORY!!\n ");
-          break;
-        }
+          print_inventory();
+        break;
+        
         case 3:
-        {
-          printf("\nUPDATE INVENTORY!!\n ");
-          inventory_updation();
-          break;
-        }
-        case 4:
-        {
-          exit(0);
-        }
-      }
-    } while (s != 4);
-  }
+        break;
 
+        case 4:
+          exit(0);
+        break;
+
+        default: 
+          printf("Wrong Input");
+        break;
+      }
+    } while (s != 3 && s!=4);
+  }
   else
   {
     printf("INVALID ID ");
