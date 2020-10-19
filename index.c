952c255b9c9b2fb137cd3aa76726ef0b91b64da7
rishit-desai <rishit-desai@users.noreply.github.com>
2020-10-19 10:30:52 +0530

    Everything works except donations for multiple members

--- index.c
+++ index.c
@@ -6,15 +6,15 @@
 int indx = -1;
 int employee_num;
 int member_id = -1;
-int s, num, k, m, q, t,i;
+int s, num, k, m, q, t, i;
 char x;
-int mem_count=0;
-int mem_index=-1;
+int mem_count = 0;
+int mem_index = -1;
 
 void press_key_to_continue()
 {
-  printf("\n\nPress any key to continue.....\n");
-  scanf("%d",&x);
+  printf("\n\nEnter any key to continue.....\n");
+  scanf("%d", &x);
 }
 
 struct INVENTORY
@@ -25,7 +25,7 @@ struct INVENTORY
   float item_MRP;
   float item_price;
   struct INVENTORY *next;
-}item[53];
+} item[53];
 
 struct INVOICE
 {
@@ -43,7 +43,7 @@ struct MEMBER
   char name[50];
   int id;
   int number_of_donation;
-}mem[100];
+} mem[100];
 
 void member_reading()
 {
@@ -80,7 +80,7 @@ void member_updation()
 {
   FILE *member;
   member = fopen("members.csv", "w");
-  for(i=0;i<mem_count;i++)
+  for (i = 0; i < mem_count; i++)
   {
     fprintf(member, "%s,%d,%d\n", mem[i].name, mem[i].id, mem[i].number_of_donation);
   }
@@ -94,7 +94,7 @@ void add_member()
   printf("Enter Member's Name: \n");
   fflush(stdin);
   gets(mem[mem_index].name);
-  mem[mem_index].id = mem_index+100;
+  mem[mem_index].id = mem_index + 100;
   mem[mem_index].number_of_donation = 0;
   member_updation();
   member_reading();
@@ -102,111 +102,99 @@ void add_member()
 
 void print_donation(int id)
 {
-  printf("THE NUMBER OF DONATIONS MADE BY THE MEMBER: %d\n",  mem[id-100].number_of_donation);
-
+  printf("THE NUMBER OF DONATIONS MADE BY THE MEMBER: %d\n", mem[id - 100].number_of_donation);
 }
 
 void afterBill()
 {
   FILE *f;
-  f=fopen("sold.csv","r");
+  f = fopen("sold.csv", "r");
   float profit;
-  char line[100],*l;
-  fgets(line,100,f);
-  l=strtok(line,"\n");
-  profit=atof(l);
-  for(int i=0;i<=indx;i++)
+  char line[100], *l;
+  fgets(line, 100, f);
+  l = strtok(line, "\n");
+  profit = atof(l);
+  for (int i = 0; i <= indx; i++)
   {
-    profit+=(item[bill.item_number[i]-101].item_MRP-item[bill.item_number[i]-101].item_price)*bill.item_q[i];
+    profit += (item[bill.item_number[i] - 101].item_MRP - item[bill.item_number[i] - 101].item_price) * bill.item_q[i];
   }
   fclose(f);
-  f=fopen("sold.csv","w");
-  fprintf(f,"%0.2f",profit);
+  f = fopen("sold.csv", "w");
+  fprintf(f, "%0.2f", profit);
   fclose(f);
 }
 
 void printProfit()
 {
   FILE *f;
-  f=fopen("sold.csv","r");
-  char line[100],*l;
-  fgets(line,100,f);
-  l=strtok(line,"\n");
-  printf("Your profit this month: %s",l);
+  f = fopen("sold.csv", "r");
+  char line[100], *l;
+  fgets(line, 100, f);
+  l = strtok(line, "\n");
+  printf("Your profit this month: %s", l);
   fclose(f);
-}
-
-void after_goodjob()
-{
-  if(!remove("employees.csv"))
-  {
-    printf("Unsuccesful");
-  }
-  if(!rename("temp.csv","employees.csv"))
-  {
-    printf("Rename");
-  }
 }
 
 void checkDate()
 {
   FILE *f;
-  f=fopen("date.txt","r");
-  int a[3],b[3];
+  f = fopen("date.txt", "r");
+  int a[3], b[3];
   SYSTEMTIME t;
   GetLocalTime(&t);
-  a[0]=t.wDay;
-  a[1]=t.wMonth;
-  a[2]=t.wYear;
+  a[0] = t.wDay;
+  a[1] = t.wMonth;
+  a[2] = t.wYear;
   char l[15];
-  fgets(l,15,f);
-  b[0]=atoi(strtok(l,"/"));
-  b[1]=atoi(strtok(NULL,"/"));
-  b[2]=atoi(strtok(NULL,"\n"));
-  fgets(l,2,f);
-  int flag=atoi(strtok(l,"\n"));
-  if(a[0]==b[0]&&flag==1)
+  fgets(l, 15, f);
+  b[0] = atoi(strtok(l, "/"));
+  b[1] = atoi(strtok(NULL, "/"));
+  b[2] = atoi(strtok(NULL, "\n"));
+  fgets(l, 2, f);
+  int flag = atoi(strtok(l, "\n"));
+  if (a[0] == b[0] && flag == 1)
   {
-    FILE *emp,*newfile;
+    FILE *emp, *newfile;
     int ti;
-    emp=fopen("employees.csv","r");
-    newfile=fopen("temp.csv","w");
-    char line[100],newline[100],*temp;
-    memset(newline,0,sizeof(newline));
-    while(fgets(line,100,emp))
+    emp = fopen("employees.csv", "r");
+    newfile = fopen("temp.csv", "w");
+    char line[100], newline[100], *temp;
+    memset(newline, 0, sizeof(newline));
+    while (fgets(line, 100, emp))
     {
-      temp=strtok(line,",");
-      strcat(newline,temp);
-      strcat(newline,",");
-      temp=strtok(NULL,",");
-      ti=0;
-      sprintf(temp,"%d",ti);
-      strcat(newline,temp);
-      strcat(newline,",");
-      strcat(newline,strtok(NULL,"\n"));
-      strcat(newline,"\n");
-      fprintf(newfile,newline);
-      memset(newline,0,sizeof(newline));
+      temp = strtok(line, ",");
+      strcat(newline, temp);
+      strcat(newline, ",");
+      temp = strtok(NULL, ",");
+      ti = 0;
+      sprintf(temp, "%d", ti);
+      strcat(newline, temp);
+      strcat(newline, ",");
+      strcat(newline, strtok(NULL, "\n"));
+      strcat(newline, "\n");
+      fprintf(newfile, newline);
+      memset(newline, 0, sizeof(newline));
     }
     fclose(emp);
     fclose(newfile);
     fclose(f);
     FILE *n;
-    n=fopen("date.txt","w");
-    fprintf(n,"%d/%d/%d\n0",b[0],b[1],b[2]);
+    n = fopen("date.txt", "w");
+    fprintf(n, "%d/%d/%d\n0", b[0], b[1], b[2]);
     fclose(n);
-    after_goodjob();
-    n=fopen("sold.csv","w");
+    remove("employess.csv");
+    rename("temp.csv", "employees.csv");
+    n = fopen("sold.csv", "w");
     fprintf(n, "0");
     fclose(n);
   }
-  else if(a[0]!=b[0])
+  else if (a[0] != b[0])
   {
 
     fclose(f);
     FILE *n;
-    n=fopen("date.txt","w");
-    fprintf(n,"%d/%d/%d\n1",b[0],b[1],b[2]);
+    n = fopen("date.txt", "w");
+    fprintf(n, "%d/%d/%d\n1", b[0], b[1], b[2]);
     fclose(n);
   }
 }
@@ -214,15 +202,16 @@ void checkDate()
 int check(int id)
 {
   FILE *emp;
-  int flag=-1,temp;
-  emp=fopen("employees.csv","r");
+  int flag = -1, temp;
+  emp = fopen("employees.csv", "r");
   char line[100];
-  while(fgets(line,100,emp))
+  while (fgets(line, 100, emp))
   {
-    temp=atoi(strtok(line,","));
-    if(temp==id)
+    temp = atoi(strtok(line, ","));
+    if (temp == id)
     {
-      flag=1;break;
+      flag = 1;
+      break;
     }
   }
   return flag;
@@ -230,75 +219,67 @@ int check(int id)
 
 void goodjob(int id)
 {
-  FILE *emp,*newfile;
+  FILE *emp;
   int ti;
-  emp=fopen("employees.csv","r");
-  newfile=fopen("temp.csv","w");
-  char line[100],newline[100],*temp;
-  memset(newline,0,sizeof(newline));
-  while(fgets(line,100,emp))
+  emp = fopen("employees.csv", "r");
+  struct employee
   {
-    temp=strtok(line,",");
-    ti=atoi(temp);
-    if(ti!=id)
-    {
-      strcat(newline,temp);
-      strcat(newline,",");
-      temp=strtok(NULL,",");
-      strcat(newline,temp);
-      strcat(newline,",");
-      strcat(newline,strtok(NULL,"\n"));
-      strcat(newline,"\n");
-      fprintf(newfile,"%s",newline);
-      memset(newline,0,sizeof(newline));
-    }
-    else
-    {
-      strcat(newline,temp);
-      strcat(newline,",");
-      temp=strtok(NULL,",");
-      ti=1+atoi(temp);
-      sprintf(temp,"%d",ti);
-      strcat(newline,temp);
-      strcat(newline,",");
-      strcat(newline,strtok(NULL,"\n"));
-      strcat(newline,"\n");
-      fprintf(newfile,"%s",newline);
-      memset(newline,0,sizeof(newline));
-    }
+    char name[50];
+    int id;
+    int number_of_sales;
+  } e[3];
+  int i = 0;
+  char line[100], *temp;
+  while (fgets(line, 100, emp))
+  {
+    temp = strtok(line, ",");
+    e[i].id = atoi(temp);
+
+    temp = strtok(NULL, ",");
+    e[i].number_of_sales = atoi(temp);
+
+    temp = strtok(NULL, "\n");
+    strcpy(e[i].name, temp);
+    i++;
+  }
+  e[id - 100].number_of_sales += 1;
+  fclose(emp);
+  emp = fopen("employees.csv", "w");
+  for (i = 0; i < 3; i++)
+  {
+    fprintf(emp, "%d,%d,%s\n", e[i].id, e[i].number_of_sales, e[i].name);
   }
   fclose(emp);
-  fclose(newfile);
 }
 
 void showsales(int id)
 {
-  
+
   FILE *emp;
   int temp;
-  emp=fopen("employees.csv","r");
-  char line[100],*l;
-  while(fgets(line,100,emp))
+  emp = fopen("employees.csv", "r");
+  char line[100], *l;
+  while (fgets(line, 100, emp))
   {
-    l = strtok(line,",");
-    int emp_id=atoi(l);
+    l = strtok(line, ",");
+    int emp_id = atoi(l);
 
-    l = strtok(NULL,",");
+    l = strtok(NULL, ",");
     int emp_sales = atoi(l);
 
-    l = strtok(NULL,"\n");
+    l = strtok(NULL, "\n");
     char name[50];
-    strcpy(name,l);
-    if(emp_id == id)
+    strcpy(name, l);
+    if (emp_id == id)
     {
-      printf("%20d%20s%20d\n",emp_id,name,emp_sales);
+      printf("%20d%20s%20d\n", emp_id, name, emp_sales);
     }
   }
 }
 
 bool check_if_possible(int item_number, int quantity)
 {
-  if(item[item_number-101].item_quantity >=  quantity)
+  if (item[item_number - 101].item_quantity >= quantity)
   {
     return true;
   }
@@ -316,27 +297,27 @@ void inventory_reading()
   int i = 0;
 
   data = fopen("inventory.csv", "r");
-  if(data == NULL)
+  if (data == NULL)
   {
-      printf("Error opening file\n");
+    printf("Error opening file\n");
   }
   else
   {
-    while(fgets(line,100,data) != NULL)
+    while (fgets(line, 100, data) != NULL)
     {
-      field = strtok(line,",");
-      strcpy(item[i].item_name,field);
+      field = strtok(line, ",");
+      strcpy(item[i].item_name, field);
 
-      field = strtok(NULL,",");
+      field = strtok(NULL, ",");
       item[i].item_no = atoi(field);
 
-      field = strtok(NULL,",");
+      field = strtok(NULL, ",");
       item[i].item_quantity = atoi(field);
 
-      field = strtok(NULL,",");
+      field = strtok(NULL, ",");
       item[i].item_MRP = atof(field);
 
-      field = strtok(NULL,"\n");
+      field = strtok(NULL, "\n");
       item[i].item_price = atof(field);
       i++;
     }
@@ -349,16 +330,16 @@ void inventory_updation()
   FILE *data;
   data = fopen("inventory.csv", "w");
 
-  if(!data)
+  if (!data)
   {
     printf("Error while updating file\n");
   }
   else
   {
 
-    for(i=0;i<53;i++)
+    for (i = 0; i < 53; i++)
     {
-      fprintf(data , "%s,%d,%d,%0.2f,%0.2f\n", item[i].item_name, item[i].item_no,item[i].item_quantity,item[i].item_MRP, item[i].item_price);
+      fprintf(data, "%s,%d,%d,%0.2f,%0.2f\n", item[i].item_name, item[i].item_no, item[i].item_quantity, item[i].item_MRP, item[i].item_price);
     }
   }
   fclose(data);
@@ -367,7 +348,7 @@ void inventory_updation()
 void update_item_quantity_after_sale()
 {
 
-  for(i=0;i<=indx;i++)
+  for (i = 0; i <= indx; i++)
   {
     item[bill.item_number[i] - 101].item_quantity = item[bill.item_number[i] - 101].item_quantity - bill.item_q[i];
   }
@@ -375,11 +356,11 @@ void update_item_quantity_after_sale()
 
 void add_item(int id, int quantity)
 {
-  if(check_if_possible(id,quantity))
+  if (check_if_possible(id, quantity))
   {
     indx++;
-    bill.item_q[indx]=quantity;
-    bill.item_number[indx]=id;
+    bill.item_q[indx] = quantity;
+    bill.item_number[indx] = id;
   }
   else
   {
@@ -391,7 +372,7 @@ void add_item(int id, int quantity)
 void print_bill()
 {
   char name[50];
-  if(member_id == -1)
+  if (member_id == -1)
   {
     printf("Enter Customer Name: ");
     fflush(stdin);
@@ -399,41 +380,39 @@ void print_bill()
   }
   else
   {
-    printf("%d%s", mem[member_id - 100].id, mem[member_id - 100].name);
     strcpy(name, mem[member_id - 100].name);
-    Sleep(1500);
   }
-  for(i=0;i<=indx;i++)
+  for (i = 0; i <= indx; i++)
   {
-      bill.subtotal += item[bill.item_number[i]-101].item_MRP*bill.item_q[i];
+    bill.subtotal += item[bill.item_number[i] - 101].item_MRP * bill.item_q[i];
   }
-  bill.gst=bill.subtotal*0.18;
-  bill.total=bill.subtotal+bill.gst;
+  bill.gst = bill.subtotal * 0.18;
+  bill.total = bill.subtotal + bill.gst;
 
-  char a[]="Item Name";
-  char b[]="Item Number";
-  char c[]="Quantity";
-  char d[]="Rate";
-  char e[]="Amount";
-  char f[]="Total";
-  char g[]="Subtotal";
-  char h[]="GST";
+  char a[] = "Item Name";
+  char b[] = "Item Number";
+  char c[] = "Quantity";
+  char d[] = "Rate";
+  char e[] = "Amount";
+  char f[] = "Total";
+  char g[] = "Subtotal";
+  char h[] = "GST";
   system("cls");
   printf("\n________________________________________________________________________________________________________\n");
   printf("                                                Invoice\n");
   printf("________________________________________________________________________________________________________\n");
-  printf("Bill to: %s\n",name);
+  printf("Bill to: %s\n", name);
   printf("|--------------------------------------------------------------------------------------------------------|\n");
-  printf("|%20s|%20s|%20s|%20s|%20s|\n",b,a,c,d,e);
-  for(i=0;i<=indx;i++)
+  printf("|%20s|%20s|%20s|%20s|%20s|\n", b, a, c, d, e);
+  for (i = 0; i <= indx; i++)
   {
     printf("|%20d|%20s|%20d|%20.2f|%20.2f|\n", item[bill.item_number[i] - 101].item_no, item[bill.item_number[i] - 101].item_name, bill.item_q[i], item[bill.item_number[i] - 101].item_MRP, item[bill.item_number[i] - 101].item_MRP * bill.item_q[i]);
   }
   printf("|--------------------------------------------------------------------------------------------------------|\n");
   printf("                                                                                                __________\n");
-  printf("%86s%20.2f\n",g,bill.subtotal);
-  printf("%86s%20.2f\n",h,bill.gst);
-  printf("%86s%20.2f\n",f,bill.total);
+  printf("%86s%20.2f\n", g, bill.subtotal);
+  printf("%86s%20.2f\n", h, bill.gst);
+  printf("%86s%20.2f\n", f, bill.total);
   printf("                                                                                                __________\n");
 }
 
@@ -442,11 +421,11 @@ void print_inventory()
   char a[] = "Item Code";
   char b[] = "Item Name";
   char c[] = "Quantity Left";
-  printf("%10s%20s%20s\n",a,b,c);
+  printf("%10s%20s%20s\n", a, b, c);
 
-  for(i=0;i<53;i++)
+  for (i = 0; i < 53; i++)
   {
-    printf("%10d%20s%20d\n",item[i].item_no,item[i].item_name,item[i].item_quantity);
+    printf("%10d%20s%20d\n", item[i].item_no, item[i].item_name, item[i].item_quantity);
   }
 }
 
@@ -464,7 +443,7 @@ void make_bill()
     scanf("%d", &m);
     if (m != 0 && m != 1)
     {
-      if(m < 101 || m > 153)
+      if (m < 101 || m > 153)
       {
         printf("\nItem Does not exist...\n");
         Sleep(1500);
@@ -477,14 +456,14 @@ void make_bill()
         flag = 1;
       }
     }
-    if(m == 1)
+    if (m == 1)
     {
       flag = 0;
     }
     system("cls");
-  } while (m != 0 && m != 1 );
+  } while (m != 0 && m != 1);
 
-  if(m == 0)
+  if (m == 0)
   {
     print_bill();
     update_item_quantity_after_sale();
@@ -495,7 +474,7 @@ void make_bill()
       scanf("%d", &flag);
       if (flag == 1)
       {
-        printf("%d",member_id);
+        mem[member_id - 100].number_of_donation++;
         member_updation();
       }
       else
@@ -504,8 +483,6 @@ void make_bill()
       }
     }
     goodjob(employee_num);
-    after_goodjob();
-    afterBill();
     press_key_to_continue();
     indx = -1;
     member_id = -1;
@@ -544,12 +521,12 @@ void call()
             printf(" \nExisting Member\n");
             printf("Id of member: ");
             scanf("%d", &member_id);
-            if(member_id == 0)
+            if (member_id == 0)
             {
               flag = 1;
               break;
             }
-            else if(member_id > mem_count*100 || member_id < 100)
+            else if (member_id > mem_count * 100 || member_id < 100)
             {
               printf("\nMember not found ):\n");
               Sleep(1500);
@@ -559,7 +536,7 @@ void call()
               flag = 1;
               make_bill();
             }
-          }while(!flag);
+          } while (!flag);
         }
         if (k == 2)
         {
@@ -569,33 +546,33 @@ void call()
             system("cls");
             printf("Enter (0) to Go Back\n");
             printf("Add Member?\n1. Yes\n2. No\n");
-            scanf("%d",&z);
-          }while(z != 1 && z != 2 && z != 0);
-          if(z == 1)
+            scanf("%d", &z);
+          } while (z != 1 && z != 2 && z != 0);
+          if (z == 1)
           {
             add_member();
             make_bill();
           }
-          else if(z == 2)
+          else if (z == 2)
           {
             make_bill();
           }
         }
         system("cls");
-      break;
+        break;
 
       case 2:
         print_inventory();
         press_key_to_continue();
         system("cls");
-      break;
+        break;
 
       case 3:
         printf("Going to Employee Login....\n");
         employee_num = 7;
         Sleep(2000);
         system("cls");
-      break;
+        break;
 
       case 4:
         system("cls");
@@ -633,7 +610,7 @@ void employee_Login()
       Sleep(2000);
       exit(0);
     }
-    else if(employee_num != 0)
+    else if (employee_num != 0)
     {
       call();
     }
@@ -647,52 +624,52 @@ void start()
     system("cls");
     printf("         Welcome\n\n");
     printf("1.Employee Login\n2.Check Profits\n3.Check Employee Sales\n4.Check Inventory\n5.Exit\n");
-    scanf("%d",&s);
-    switch(s)
+    scanf("%d", &s);
+    switch (s)
     {
-      case 1:
-        system("cls");
-        employee_Login();
+    case 1:
+      system("cls");
+      employee_Login();
       break;
 
-      case 2:
-        system("cls");
-        printProfit();
-        press_key_to_continue();
+    case 2:
+      system("cls");
+      printProfit();
+      press_key_to_continue();
       break;
 
-      case 3: 
-        system("cls");
-        char a[] = "Employee Id";
-        char b[] = "Employee Name";
-        char c[] = "Number of sales by employee";
-        printf("%20s%20s%20s\n", a, b, c);
-        showsales(100);
-        showsales(101);
-        showsales(102);
-        press_key_to_continue();
+    case 3:
+      system("cls");
+      char a[] = "Employee Id";
+      char b[] = "Employee Name";
+      char c[] = "Number of sales";
+      printf("%20s%20s%20s\n", a, b, c);
+      showsales(100);
+      showsales(101);
+      showsales(102);
+      press_key_to_continue();
       break;
 
-      case 4:
-        system("cls");
-        print_inventory();
-        press_key_to_continue();
+    case 4:
+      system("cls");
+      print_inventory();
+      press_key_to_continue();
       break;
 
-      case 5:
-        system("cls");
-        printf("Have a nice day!!!!");
-        Sleep(1500);
-        exit(0);
+    case 5:
+      system("cls");
+      printf("Have a nice day!!!!");
+      Sleep(1500);
+      exit(0);
       break;
 
-      default: 
-        printf("\n\nWrong Input\n");
-        Sleep(2000);
-        system("cls");
+    default:
+      printf("\n\nWrong Input\n");
+      Sleep(2000);
+      system("cls");
       break;
     }
-  }while(s != 5);
+  } while (s != 5);
 }
 
 int main()
